5c3bd3c40f60555934ddb26c8ccc97e3a3d9af41
Sergey Belov <kort.dll@gmail.com>
2023-06-14 13:12:41 +0300

    remove unused MessagePattern

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('user')
-  @MessagePattern({ cmd: 'get-user' })
   @UseGuards(JwtAuthGuard)
   async getUser(@Request() req) {
     return {
