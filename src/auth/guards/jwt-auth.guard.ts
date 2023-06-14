7c0f855fb0837c67b61ae7817d22ede0fcefc9da
Sergey Belov <kort.dll@gmail.com>
2023-06-14 12:47:23 +0300

    fix throwable error

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { BadTokenException } from '../errors/bad-token-exception';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      super.canActivate(context);
+    } catch (e) {
+      throw new BadTokenException();
+    }
+
+    return true;
+  }
+}
